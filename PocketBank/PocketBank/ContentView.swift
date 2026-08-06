7fbed6b2435aa60997a7c62372feca6c5ad0b936
karmic <karbemichael@gmail.com>
2026-08-06 14:08:48 +0200

    Add empty project

---
+++ PocketBank/PocketBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketBank
+//
+//  Created by karmic on 8/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketBank/PocketBank/PocketBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBankApp.swift
+//  PocketBank
+//
+//  Created by karmic on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PocketBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
